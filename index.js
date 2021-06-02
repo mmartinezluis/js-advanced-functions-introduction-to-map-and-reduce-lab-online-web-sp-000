08a91abada6a568fd83ad9ef00b51f8d864dcbd3
Luis Martinez <luis_mmartinez@live.com>
2021-06-02 17:18:34 -0400

    commit tree 7c9dedda38bda87b1a4515518de7f7104f2238d9

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Your code here
+function mapToNegativize(array) {
+    let newArray = [];
+    array.forEach(element => newArray.push(-1*element));
+    return newArray;
+}
+
+function mapToNoChange(array) {
+    return array.slice();
+}
+
+function mapToDouble(array) {
+    let newArray = [];
+    array.forEach(function(element) {
+        newArray.push(2*element);
+    });
+    return newArray;
+}
+
+function mapToSquare(array) {
+    let newArray = [];
+    for(let i=0; i< array.length; i++) {
+        newArray.push((array[i])**2);
+    };
+    return newArray;
+}
+
+function reduceToTotal(array, startingPoint = 0) {
+  let total = 0;
+  array.forEach( e => total = total + e);
+  return total + startingPoint;
+}
+
+function reduceToAllTrue(array) {
+  let newArray = [];
+  array.forEach( e => newArray.push(!!e));
+  return !newArray.includes(false);
+};
+  
+function reduceToAnyTrue(array){
+  let newArray = [];
+  array.forEach( e => newArray.push(!!e));
+  return newArray.includes(true);
+};
